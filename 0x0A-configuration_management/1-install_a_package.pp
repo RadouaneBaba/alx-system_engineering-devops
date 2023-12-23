fe08fad5774ee8e2ad5e225967ea1840524e801d
RadouaneBaba <radoine.baba04@gmail.com>
2023-12-23 02:37:16 -0800

    test4

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # install flask from pip3
-exec {'flask_install':
-  command => 'pip3 install Flask==2.1.0',
-  path    => ['/usr/bin']
+exec { 'install_flask_2.1.0':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
