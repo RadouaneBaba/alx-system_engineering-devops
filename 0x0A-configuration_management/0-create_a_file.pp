589dcb6f59c84f10e7ff33443dc0b705bbf8b6fa
RadouaneBaba <radoine.baba04@gmail.com>
2023-12-22 16:18:08 -0800

    tst

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,8 @@
 # Create file in /tmp
 
-file {
-  path => '/tmp/school',
+file {'/tmp/school':
   ensure => present,
-  mode => 0744,
+  mode => '0744',
   owner => 'www-data',
   group => 'www-data',
   content => 'I love Puppet'
