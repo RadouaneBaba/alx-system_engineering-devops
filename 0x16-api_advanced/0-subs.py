a67f99435065f7ae49db40d380e915739893f621
RadouaneBaba <radoine.baba04@gmail.com>
2024-02-13 06:15:54 -0800

    test2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,8 @@ import requests
 def number_of_subscribers(subreddit):
     """ get number of subscriberts of subreddit """
     headers = {'User-Agent': 'my-app/0.0.1'}
-    res = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit), headers=headers)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
     if res.status_code != 200:
         return 0
     else:
