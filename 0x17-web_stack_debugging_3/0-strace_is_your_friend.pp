230282e44bba8c4c427ca7d536d81f1e2f5e3932
RadouaneBaba <radoine.baba04@gmail.com>
2024-02-14 07:09:24 -0800

    test fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 # this puppet will fix a typo in wp settings file
 exec { '/var/www/html/wp-settings.php':
-  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  command => "/bin/sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
 }
