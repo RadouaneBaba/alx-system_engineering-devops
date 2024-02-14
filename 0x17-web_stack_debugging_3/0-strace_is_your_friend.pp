db6c6756f303cd0c4bf8c71d8ed32e5d455d4870
RadouaneBaba <radoine.baba04@gmail.com>
2024-02-14 07:04:28 -0800

    correct

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,4 @@
 # this puppet will fix a typo in wp settings file
-file {  '/var/www/html/wp-settings.php':
-  ensure => present,
-}->
-file_line { 'change php to phpp':
-  path => '/var/www/html/wp-settings.php',
-  line => "require_once( ABSPATH . WPINC . '/class-wp-locale.php' );",
-  match => "require_once( ABSPATH . WPINC . '/class-wp-locale.phpp' );",
+exec { '/var/www/html/wp-settings.php':
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
 }
